9e99b219a0b5f661c2500068fa4f69af9f593b42
VanTalles <63775368+VanTalles@users.noreply.github.com>
2020-12-16 08:55:21 +0300

    Update script.py

--- script.py
+++ script.py
@@ -27,32 +27,36 @@ nb = pynetbox.api(NB_URL, token = NB_TOKEN)
 
 dl = getList4CSV('devices.csv')
 for dev in dl:
+    model_slug = dev['model'].lower()
     model_id = nb.dcim.device_types.get(slug = dev['model'].lower())
-    dev_name = dev['name']
-    #print (f'{dev_name}          {str(model_id.id)}')
-    item = dict (
-        name = dev['name'],
-        device_type = model_id.id,
-        device_role = DEVICE_ROLE,
-        tenant = TENANT,
-        site = SITE,
-    )
-    dev_new = nb.dcim.devices.create(item)
-    print (dev_new)
-    item_interface = dict (
-        device = dev_new.id,
-        name = "VLAN"+str(dev['vlan']),
-        type = 'virtual',
-    )
-    dev_int_new = nb.dcim.interfaces.create(item_interface)
-    print (dev_int_new.id)
-    item_ip = dict (
-        family = 4,
-        address = dev["ip"]+'/24',
-        tenant = TENANT,
-        status = 'active',
-        assigned_object_id = dev_int_new.id,
-        assigned_object_type = 'dcim.interface',
-    )
+    print ('='*50)
+    if model_id:
+        dev_name = dev['name']
+        print (f'Import {dev_name}    as      {str(model_id.model)}')
+        item = dict (
+            name = dev['name'],
+            device_type = model_id.id,
+            device_role = DEVICE_ROLE,
+            tenant = TENANT,
+            site = SITE,
+        )
+        dev_new = nb.dcim.devices.create(item)       
+        item_interface = dict (
+            device = dev_new.id,
+            name = "VLAN"+str(dev['vlan']),
+            type = 'virtual',
+        )
+        dev_int_new = nb.dcim.interfaces.create(item_interface)
+        item_ip = dict (
+            family = 4,
+            address = dev["ip"]+'/24',
+            tenant = TENANT,
+            status = 'active',
+            assigned_object_id = dev_int_new.id,
+            assigned_object_type = 'dcim.interface',
+        )
+        iip = nb.ipam.ip_addresses.create(item_ip)
 
-    iip = nb.ipam.ip_addresses.create(item_ip)
+    else:
+        print (f'Not found device type with slug {model_slug}')
+        
